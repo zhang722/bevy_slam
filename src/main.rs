84a4ad65fa92c5fd235330fefb1e79ffcaa17c28
zhang722 <1755169086@qq.com>
2023-05-15 21:51:44 +0800

    send message loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::io::{self, Write};
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
 mod slam;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    // 连接到目标 TCP 服务器
+    let mut stream = TcpStream::connect("127.0.0.1:9123")?;
+    let mut i = 0;
+
+    // 循环发送消息
+    loop {
+        let message = format!("Hello, TCP server{}!\n", i);
+        i += 1;
+        // let message = "Hello, TCP server!";
+        stream.write_all(message.as_bytes())?;
+
+        // 暂停一段时间
+        thread::sleep(Duration::from_secs(1));
+    }
 }
